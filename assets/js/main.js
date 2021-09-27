bd60198d08046f60805a5c1c62dd08e4ae453f4f
Fandel-Lin <fandel123@gmail.com>
2021-09-27 19:11:56 +0800

    update content

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,10 +177,36 @@
 			modal.style.display = "none";
 		}
 
+
+		
+		
+		// Get the modal
+		var modal2 = document.getElementById("myResearch");
+
+		// Get the button that opens the modal
+		var btn2 = document.getElementById("myBtn2");
+
+		// Get the <span> element that closes the modal
+		var span2 = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the modal 
+		btn2.onclick = function() {
+			modal2.style.display = "block";
+		}
+		
+
+		
+		
 		// When the user clicks anywhere outside of the modal, close it
 		window.onclick = function(event) {
 			if (event.target == modal) {
 				modal.style.display = "none";
 			}
+			if (event.target == modal2) {
+				modal2.style.display = "none";
+			}
 		}
+		
+		
+		
 })(jQuery);
