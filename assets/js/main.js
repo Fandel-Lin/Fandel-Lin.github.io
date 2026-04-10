70c822e503b272ff5ec1dc840e02f6712797d2c9
Fandel Lin <fandel123@gmail.com>
2026-04-09 19:47:49 -0700

    Update index with a picture
    Update the index with the sitemap, and upload a new picture.

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,6 +219,22 @@
 		
 		
 		
+		// Get the modal
+		var modal4 = document.getElementById("myDefensePic");
+
+		// Get the button that opens the modal
+		var btn4 = document.getElementById("myBtn4");
+
+		// Get the <span> element that closes the modal
+		var span4 = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the modal 
+		btn4.onclick = function() {
+			modal3.style.display = "block";
+		}
+		
+		
+		
 		// When the user clicks anywhere outside of the modal, close it
 		window.onclick = function(event) {
 			if (event.target == modal) {
@@ -229,6 +245,9 @@
 			}
 			if (event.target == modal3) {
 				modal3.style.display = "none";
+			}
+			if (event.target == modal4) {
+				modal4.style.display = "none";
 			}
 		}
 		
